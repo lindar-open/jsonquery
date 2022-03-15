1cfde0a6fc47788f8d536e2c43973a9299fb2a3d
Steven Hills <steven.hills@gmail.com>
2022-03-15 14:38:16 +0000

    fix: empty predicate check

--- jsonquery-querydsl-jpa/src/main/java/com/lindar/jsonquery/querydsl/jpa/Util.java
+++ jsonquery-querydsl-jpa/src/main/java/com/lindar/jsonquery/querydsl/jpa/Util.java
@@ -1,15 +1,21 @@
 package com.lindar.jsonquery.querydsl.jpa;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
 
 /**
  * Created by Steven on 30/09/2016.
  */
 public class Util {
-    public static boolean isPredicateEmpty(Predicate predicate){
-        if((predicate == null || "".equals(predicate.toString()))){
+    public static boolean isPredicateEmpty(Predicate predicate) {
+        if (predicate == null) {
             return true;
         }
-        return false;
+
+        if (predicate instanceof BooleanBuilder) {
+            return !((BooleanBuilder) predicate).hasValue();
+        }
+
+        return "".equals(predicate.toString());
     }
 }
