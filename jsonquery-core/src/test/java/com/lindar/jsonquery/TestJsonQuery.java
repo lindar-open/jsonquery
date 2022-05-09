af5dc56a494eac8869ef19c272c546f5086d752a
Steven Hills <steven.hills@gmail.com>
2022-05-09 19:46:13 +0100

    test: check serialising and deserialising queries

--- jsonquery-core/src/test/java/com/lindar/jsonquery/TestJsonQuery.java
+++ jsonquery-core/src/test/java/com/lindar/jsonquery/TestJsonQuery.java
@@ -1,18 +1,15 @@
 package com.lindar.jsonquery;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.Lists;
 import com.lindar.jsonquery.ast.LogicalNode;
-import com.lindar.jsonquery.ast.StringComparisonNode;
-import com.lindar.jsonquery.ast.Node;
-import com.lindar.jsonquery.ast.StringComparisonOperation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by stevenhills on 24/09/2016.
@@ -23,23 +20,19 @@ public class TestJsonQuery {
     private ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
-    public void test() throws JsonProcessingException {
-
-        StringComparisonNode stringNode = new StringComparisonNode();
-        stringNode.setField("username");
-        stringNode.setOperation(StringComparisonOperation.BEGINS_WITH);
-        ArrayList<String> values = new ArrayList<String>();
-        values.add("random");
-        stringNode.setValue(values);
-
-        ArrayList<Node> nodes = Lists.newArrayList();
-        nodes.add(stringNode);
-
+    public void writeAndReadEmptyQuery() throws IOException {
         LogicalNode andNode = new LogicalNode(LogicalNode.LogicalOperation.AND);
-        andNode.setItems(nodes);
+        andNode.setReference("test-reference");
+        String json = objectMapper.writeValueAsString(andNode);
 
-        System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(andNode));
+        LogicalNode logicalNode = objectMapper.readValue(json, LogicalNode.class);
+        assertEquals("test-reference", logicalNode.getReference());
+    }
 
-        BigDecimal bigDecimal;
+    @Test
+    public void readEmptyQuery() throws IOException {
+        String query = "{\"type\":\".LogicalNode\",\"reference\":\"test-reference\",\"enabled\":true,\"operation\":\"AND\",\"items\":[]}";
+        LogicalNode logicalNode = objectMapper.readValue(query, LogicalNode.class);
+        assertNotNull(logicalNode);
     }
 }
